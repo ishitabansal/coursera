d1967cb7ed001f6f9b134fe85d792f65df44c177
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-08-04 23:24:14 +0530

    after adding some line

--- example1.py
+++ example1.py
@@ -1,4 +1,4 @@
  def git_opeation():      //define function
   
- print("I am adding example.py file to the remote repository.")
+ print("I am adding example.py file to the remote repository. and i wnt learn the git more")
  git_opeation()
